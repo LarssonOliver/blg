75640e4749cde1d039b0a28fba7660d520a7a7c9
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-01-07 14:28:46 +0100

    Work in progress

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,22 @@
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  modules: [
+    "@nuxt/content",
+  ],
+  content: {
+    highlight: {
+      preload: [
+        "diff",
+        "json",
+        "js",
+        "ts",
+        "css",
+        "shell",
+        "html",
+        "md",
+        "yaml",
+      ],
+      theme: "nord",
+    },
+  },
+});
---
+++ pages/[...slug].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const { path } = useRoute();
+const { data } = await useAsyncData(`content-${path}`, () => {
+  return queryContent().where({ _path: path }).only(['title']).findOne()
+})
+console.log(data);
+</script>
+
+<template>
+  <main>
+    <!-- stuff here -->
+    <article>
+      <ContentDoc />
+    </article>
+  </main>
+</template>
+
+<style scoped>
+  article {
+    width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+</style>
