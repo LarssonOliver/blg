b270414bf04d40ec7ac2835841fa3c96ef6fe208
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-06-24 22:22:11 +0200

    Html metadata

--- app.vue
+++ app.vue
@@ -4,6 +4,27 @@
   <TheFooter />
 </template>
 
-<script setup>
+<script setup lang="ts">
 import "@/assets/index.scss";
+
+useHead({
+  title: 'Oliver Larsson',
+  meta: [
+    { name: 'description', content: "Oliver Larsson's personal blog." },
+    { name: 'lang', content: 'en' },
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  },
+});
+
+useSeoMeta({
+  title: 'Oliver Larsson',
+  ogTitle: 'Oliver Larsson',
+  author: 'Oliver Larsson',
+  description: "Oliver Larsson's personal blog.",
+  ogDescription: "Oliver Larsson's personal blog.",
+//  ogImage: 'https://example.com/image.png',
+//  twitterCard: 'summary_large_image',
+});
 </script>
